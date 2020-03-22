1504937d600b76ebfac0be081b2f1a2a0a58a01a
Oleg Sandro <okuziura@gmail.com>
2020-03-23 00:21:01 +0300

    JSONSMPL-002 Add entity

---
+++ src/Rectangle.java
@@ -0,0 +1,29 @@
+public class Rectangle {
+
+    private int length;
+    private int width;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
