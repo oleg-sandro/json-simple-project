85902a2c3d9ad0a5ef4bbe03a9f96e231b5d874a
Oleg Sandro <okuziura@gmail.com>
2020-03-23 00:41:34 +0300

    JSONSMPL-003 Add all classes to a package

---
+++ src/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle.toString());
+    }
+}
